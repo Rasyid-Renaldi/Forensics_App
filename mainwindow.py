bf4ecbdff752a4ca56afa13de5cf2d790c05b2ae
Rasyid-deve <renaldirasyed@gmail.com>
2023-04-01 10:56:45 +0700

    updated

--- mainwindow.py
+++ mainwindow.py
@@ -48,3 +48,48 @@ class MainWindow (QMainWindow, Ui_MainWindow):
     #     if fileDialog.exec_():
     #         selected_file = fileDialog.selectedFiles()[0]
     #         print("Selected file:", selected_file)
+
+# import PySide6.QtCore as QtCore
+# import PySide6.QtGui as QtGui
+# import PySide6.QtWidgets as QtWidgets
+
+# class ScanDevice(QtWidgets.QDialog):
+#     def __init__(self, parent=None):
+#         super().__init__(parent)
+ 
+#         # UI setup
+#         layout = QtWidgets.QVBoxLayout(self)
+#         self.textbox = QtWidgets.QLineEdit(self)
+#         self.textbox.setPlaceholderText('Insert device name')
+#         layout.addWidget(self.textbox)
+#         button = QtWidgets.QPushButton('Scan', self)
+#         button.clicked.connect(self.scan_device)
+#         layout.addWidget(button)
+ 
+#     def scan_device(self):
+#         device_name = self.textbox.text()
+ 
+#         # Scan for device
+#         device_found = False
+#         devices = QtCore.QBluetoothDeviceDiscoveryAgent()
+#         devices.deviceDiscovered.connect(lambda device: self.device_discovered(device, device_name))
+#         devices.start()
+#         devices.finished.connect(self.scan_finished)
+ 
+#         # Show scanning progress
+#         self.progress = QtWidgets.QProgressBar(self)
+#         self.progress.setRange(0, 0)
+#         self.progress.setValue(0)
+#         self.progress.show()
+ 
+#     def device_discovered(self, device_info, device_name):
+#         if device_info.name() == device_name:
+#             self.progress.close()
+#             self.textbox.clear()
+#             QtWidgets.QMessageBox.information(self, 'Device Found', f'{device_name} found at {device_info.address().toString()}')
+#             self.accept()
+ 
+#     def scan_finished(self):
+#         self.progress.close()
+#         QtWidgets.QMessageBox.warning(self, 'Error', 'Device not found')
+#         self.reject()
