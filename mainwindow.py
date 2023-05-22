4f788e91d5421d704dd829f5af9804befde9fa4f
Rasyid-deve <renaldirasyed@gmail.com>
2023-05-23 06:34:32 +0700

    update

--- mainwindow.py
+++ mainwindow.py
@@ -1,13 +1,15 @@
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QStringListModel
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime,
                             QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
 from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase,
                            QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
-from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog, QLabel, QGraphicsDropShadowEffect, QProgressBar, QComboBox
+from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog, QLabel, QGraphicsDropShadowEffect, QProgressBar, QComboBox, QListView
 import time
+import enum
 import os
 import pandas as pd
+from pytesseract import pytesseract
 from PIL import Image
 from openpyxl import Workbook
 
@@ -15,15 +17,25 @@ from main_ui import Ui_Aplikasi_Forensik
 from splash_screen_ui import Ui_SplashScreen
 
 
+class OS(enum.Enum):
+    Windows = 1
+
+
+class Language(enum.Enum):
+    ENG = 'eng'
+    IND = 'ind'
+
+
 class Aplikasi_Forensik (QMainWindow, Ui_Aplikasi_Forensik):
     def __init__(self, app):
         super().__init__()
         self.setupUi(self)
         self.app = app
+        self.text_file_path = None
 
         self.detectDevice.currentIndexChanged.connect(self.dialog_connect)
-        self.convertButton.clicked.connect(self.start_convert_image)
         # self.convertButton.clicked.connect(self.convert_image_to_excel)
+        self.convertButton.clicked.connect(self.start_convert_image)
 
         self.actionQuit.triggered.connect(self.quit)
         self.actionAbout_App.triggered.connect(self.about)
@@ -35,74 +47,52 @@ class Aplikasi_Forensik (QMainWindow, Ui_Aplikasi_Forensik):
         self.scanButton.clicked.connect(self.dialog_scan)
         self.cekButton.clicked.connect(self.cek_button)
 
-        self.okButton.clicked.connect(self.exit)
-        # self.convertButton.clicked.connect(self.start)
+        self.activity_model = QStringListModel()
+        self.viewData.setModel(self.activity_model)
 
-        # for i in range(101):
-        #     self.acquisitionBar.setValue(i)
-        #     time.sleep(0.03)
+        self.okButton.clicked.connect(self.exit)
 
     def start_convert_image(self):
         self.convertButton.setEnabled(False)
+
+        self.add_activity("Mengubah gambar menjadi teks")
+        self.add_activity("Path gambar: {}".format(self.search.text()))
+        self.add_activity("Path penyimpanan: {}".format(self.text_file_path))
+        self.add_activity("Nama examiner: {}".format(self.lineEdit.text()))
+        self.add_activity("Lokasi file output: {}".format(self.save_locations()))
 
         animation = QPropertyAnimation(self.acquisitionBar, b"value")
         animation.setDuration(3000)
         animation.setStartValue(0)
         animation.setEndValue(100)
+        # Menghubungkan ke fungsi convert_image_to_excel
+        animation.finished.connect(self.convert_image_to_text)
         animation.start()
 
-        self.convert_image_to_excel()
-    
-    def convert_image_to_excel(self):
+        self.convert_image_to_text()
+
+    def convert_image_to_text(self):
         image_path = self.search.text()
-        excel_path = self.browseLocation.text()
 
-        if image_path and excel_path:
+        if image_path and self.text_file_path:
             image = Image.open(image_path)
+            win_path = r'E:\Program Files\Tesseract-OCR\tesseract.exe'
+            pytesseract.tesseract_cmd = win_path
+            print('Run on: Windows\n')
 
-            workbook = Workbook()
-            sheet = workbook.active
+            extracted_text = pytesseract.image_to_string(image)
 
-            max_image_width = sheet.column_dimensions['A'].width
-            max_image_height = sheet.row_dimensions[1].height
-
-            default_width = 100  # Nilai lebar default
-            default_height = 100  # Nilai tinggi default
-            rel = None
-
-            try:
-                if max_image_width is not None:
-                    max_image_width = int(max_image_width)
-                else:
-                    max_image_width = default_width
-
-                if max_image_height is not None:
-                    max_image_height = int(max_image_height)
-                else:
-                    max_image_height = default_height
-
-                max_image_size = (max_image_width, max_image_height)
-                resized_image = image.resize(max_image_size)
-
-                # Add the image to the worksheet
-                image_cell = sheet.cell(row=1, column=1)
-                image_cell.value = "Image"
-                sheet.column_dimensions['A'].width = resized_image.width
-                sheet.row_dimensions[1].height = resized_image.height
-                sheet.add_image(resized_image, 'A2')
-
-                workbook.save(excel_path)
-        
-                QMessageBox.information(self, "Konversi Selesai", "Gambar berhasil dikonversi menjadi file Excel!")
-            except (ValueError, TypeError) as e:
-                rel = str(e)
-            if rel is not None:
-                QMessageBox.warning(self, "Error", "Ukuran lebar atau tinggi gambar tidak valid!\n\n" + rel)
+            with open(self.text_file_path, "w", encoding="utf-8") as text_file:
+                text_file.write(extracted_text)
+            QMessageBox.information(
+                self, "Ekstraksi Selesai", "Teks berhasil diekstrak dan disimpan dalam file teks"
+            )
         else:
-            QMessageBox.warning(self, "Error", "Mohon pilih file gambar dan lokasi penyimpanan terlebih dahulu!")
-        
+            QMessageBox.warning(
+                self, "Error", "Mohon pilih file gambar dan lokasi penyimpanan terlebih dahulu!")
+
         self.convertButton.setEnabled(True)
-        
+
     def quit(self):
         self.app.quit()
 
@@ -115,13 +105,21 @@ class Aplikasi_Forensik (QMainWindow, Ui_Aplikasi_Forensik):
 
     def search_button(self):
         search = QFileDialog.getOpenFileName(
-            self, "Open File", "D:\Skripsi", ".JPG files (*.jpg)")
+            self, "Open File", "D:\Skripsi", "JPG files (*.jpg)")
         self.search.setText(search[0])
+        self.add_activity("Path gambar: {}".format(search[0]))
 
     def save_locations(self):
-        browse = QFileDialog.getSaveFileName(
-            self, "Save File", "D:\Skripsi\image")
-        self.browseLocation.setText(browse[0])
+        selected_location = QFileDialog.getSaveFileName(
+            self, "Save File", "D:\Skripsi", "TXT files (*.txt)")
+        self.browseLocation.setText(selected_location[0])
+        self.text_file_path = selected_location[0]
+        self.add_activity("Path penyimpanan: {}".format(selected_location[0]))
+
+    def add_activity(self, activity):
+        act = self.activity_model.stringList()
+        act.append(activity)
+        self.activity_model.setStringList(act)
 
     def dialog_scan(self):
         scan = QMessageBox.question(
@@ -160,7 +158,8 @@ class Aplikasi_Forensik (QMainWindow, Ui_Aplikasi_Forensik):
         if button_clicked == QMessageBox.Yes:
             print("Ok")
         else:
-            self.result.setText("Not Root!")
+            print("No")
+            # self.result.setText("Not Root!")
 
     def exit(self):
         self.app.exit()
@@ -171,10 +170,6 @@ counter = 0
 jumper = 10
 
 # ==> SPLASHSCREEN WINDOW
-
-# untuk kebutuhan pada saat demo ketika sidang nanti
-# if selected_item == self.result.setText("Root"):
-            #     print("Telah root")
 
 class SplashScreen(QMainWindow):
     def __init__(self):
@@ -274,3 +269,53 @@ class SplashScreen(QMainWindow):
 
         # APPLY STYLESHEET WITH NEW VALUES
         self.ui.circularProgress.setStyleSheet(newStylesheet)
+
+
+# untuk kebutuhan pada saat demo ketika sidang nanti
+# if selected_item == self.result.setText("Root"):
+#     print("Telah root")
+
+# for i in range(101):
+#     self.acquisitionBar.setValue(i)
+#     time.sleep(0.03)
+# self.convertButton.clicked.connect(self.start_convert_image)
+
+ # workbook = Workbook()
+            # sheet = workbook.active
+
+            # max_image_width = sheet.column_dimensions['A'].width
+            # max_image_height = sheet.row_dimensions[1].height
+
+            # default_width = 100  # Nilai lebar default
+            # default_height = 100  # Nilai tinggi default
+
+            # try:
+            #     if max_image_width is not None:
+            #         max_image_width = int(max_image_width)
+            #     else:
+            #         max_image_width = default_width
+
+            #     if max_image_height is not None:
+            #         max_image_height = int(max_image_height)
+            #     else:
+            #         max_image_height = default_height
+
+            #     max_image_size = (max_image_width, max_image_height)
+            #     resized_image = image.resize(max_image_size)
+
+            #     image_cell = sheet.cell(row=1, column=1)
+            #     image_cell.value = "Image"
+            #     sheet.column_dimensions['A'].width = resized_image.width
+            #     sheet.row_dimensions[1].height = resized_image.height
+            #     sheet.add_image(resized_image, 'A4')
+
+            #     text_cell = sheet.cell(row=2, column=1)
+            #     text_cell.value = extracted_text
+
+            #     workbook.save(excel_path)
+
+            #     QMessageBox.information(
+            #         self, "Konversi Selesai", "Gambar berhasil dikonversi menjadi file Excel!")
+            # except (ValueError, TypeError) as e:
+            #     QMessageBox.warning(
+            #         self, "Error", "Ukuran lebar atau tinggi gambar tidak valid!\n\n{str(e)}")
